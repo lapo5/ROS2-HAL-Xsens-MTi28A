b93fe338ce54e4e636096fd2b86a41ee1fbcb318
Marco Lapolla <marco.lapolla5@gmail.com>
2021-07-20 12:47:22 +0200

    Fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('lib', package_name, 'resources'), glob('hal_xsens_mti_28a/*.so')),
+        (os.path.join('lib', package_name, 'resources'), glob(package_name+'/*.so')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
